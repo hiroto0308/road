d9c8fb87962b2368b06f477d5fb853648a9eb4fe
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2021-09-12 15:54:04 +0900

    モデルの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :nickname
+    validates :password
+  end
+
+ 
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :gender
+  belongs_to :area
+
+  has_one_attached :image
 end
