3e177ae5aa35b46ae0726c4e9ea87b50782379a9
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2021-09-20 09:22:32 +0900

    ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'static_pages#top'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+  devise_scope :user do
+   post '/users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+   root to: 'static_pages#top'
   resources :rooads
 end
