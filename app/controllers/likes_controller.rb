43394392aadbd0c90fbf7afad677e045c793126c
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2021-09-23 20:11:38 +0900

    非同期いいね機能

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,17 @@
 class LikesController < ApplicationController
+  before_action :post_params
+
   def create
     Like.create(user_id: current_user.id, rooad_id: params[:id])
-    redirect_to rooads_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, rooad_id: params[:id]).destroy
-    redirect_to rooads_path
+  end
+
+  private
+
+  def post_params
+    @rooad = Rooad.find(params[:id])
   end
 end
